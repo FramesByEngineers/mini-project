85a245a43257bd0746998800b5e191b7f87312c4
Nilesh Pandey <99646982+NileshPandey05@users.noreply.github.com>
2024-04-02 05:44:31 +0000

    added images

--- src/components/exhibition/bengrid.js
+++ src/components/exhibition/bengrid.js
@@ -9,6 +9,7 @@ import {
   IconTableColumn,
 } from "@tabler/icons-react";
 import { BentoGrid, BentoGridItem } from "@/components/ui/bento-grid";
+import Image from "next/image";
 
 export function BentoGridDemo() {
   return (
@@ -27,66 +28,67 @@ export function BentoGridDemo() {
     </BentoGrid>
   );
 }
-const Skeleton = () => (
-  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
+const Skeleton = ({img}) => (
+  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl ">
+    <img  className="w-full"   src={img}  />
+  </div>
 );
 
 const items = [
   {
     title: "The Dawn of Innovation",
     description: "Explore the birth of groundbreaking ideas and inventions.",
-    header: <Skeleton />,
+    header: <Skeleton img={'https://cdn.pixabay.com/photo/2012/08/27/14/19/mountains-55067_1280.png'}  />,
     icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
-    image:
-      "https://i.pinimg.com/564x/72/a8/d4/72a8d47192ef0c99f3c3dc3d17fd2d6e.jpg",
+    
   },
   {
     title: "The Digital Revolution",
     description: "Dive into the transformative power of technology.",
-    header: <Skeleton />,
+    header: <Skeleton img={'https://img.freepik.com/free-photo/lone-tree_181624-46361.jpg'}/>,
     icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
     image:
-      "https://i.pinimg.com/564x/50/c0/e6/50c0e61277d10524d94ed015f761237c.jpg",
+      "",
   },
   {
     title: "The Art of Design",
     description: "Discover the beauty of thoughtful and functional design.",
-    header: <Skeleton />,
+    header: <Skeleton img={'https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?size=626&ext=jpg&ga=GA1.1.2113030492.1711929600&semt=sph'}/>,
     icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
     image:
-      "https://i.pinimg.com/564x/a4/20/3d/a4203d67400a99ff27c0b64821c85e13.jpg",
+      "",
   },
   {
     title: "The Power of Communication",
     description:
       "Understand the impact of effective communication in our lives.",
-    header: <Skeleton />,
+    header: <Skeleton img={'https://iso.500px.com/wp-content/uploads/2014/07/big-one.jpg' }/>,
     icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
     image:
-      "https://i.pinimg.com/564x/4f/99/14/4f9914b95cfdca6a8098681f965b6d0e.jpg",
+      "",
   },
   {
     title: "The Pursuit of Knowledge",
     description: "Join the quest for understanding and enlightenment.",
-    header: <Skeleton />,
+    header: <Skeleton img={'https://4.imimg.com/data4/FW/EM/MY-3610322/two-ladies-500x500.jpg'}/>,
     icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-500" />,
     image:
-      "https://i.pinimg.com/736x/bd/67/39/bd67391923336a3127c6d658e3a550bf.jpg",
+      "",
   },
   {
     title: "The Joy of Creation",
     description: "Experience the thrill of bringing ideas to life.",
-    header: <Skeleton />,
+    header: <Skeleton img={'https://mylands.com/cdn/shop/articles/Thomas_Gainsborough_s_Cornard_Wood_on_loan_from_The_National_Gallery_on_show_alongside_Study_for_Conard_Wood_and_other_drawings_reattributed_to_Gainsborough_lent_by_Her_Majesty_The_Qu.jpg?v=1633518007'}/>,
     icon: <IconBoxAlignTopLeft className="h-4 w-4 text-neutral-500" />,
     image:
-      "https://i.pinimg.com/564x/72/a8/d4/72a8d47192ef0c99f3c3dc3d17fd2d6e.jpg",
+      "",
   },
   {
     title: "The Spirit of Adventure",
     description: "Embark on exciting journeys and thrilling discoveries.",
-    header: <Skeleton />,
+    header: <Skeleton img={'https://www.1st-art-gallery.com/media/wysiwyg/back-mob-min2.jpg'}/>,
     icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
     image:
-      "https://i.pinimg.com/736x/85/bf/d3/85bfd3ea513efc44bf02685213ce3e6b.jpg",
+      "",
   },
 ];
