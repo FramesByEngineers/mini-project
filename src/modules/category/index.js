036cb92d60715a52803bf8fe82b9b8407f5a8dc7
Nilesh Pandey <99646982+NileshPandey05@users.noreply.github.com>
2024-04-02 06:06:17 +0000

    category/card

--- src/modules/category/index.js
+++ src/modules/category/index.js
@@ -6,6 +6,7 @@ import Models from "@/assets/img/roll models.webp";
 import Bollywood from "@/assets/img/bollywood.jpg";
 import Link from "next/link";
 import Image from "next/image";
+import { CardHoverEffectDemo } from "./card";
 
 const CategoryPage = () => {
   return (
@@ -19,64 +20,66 @@ const CategoryPage = () => {
         Categories
       </p>
 
-      <div className="flex gap-4 flex-wrap md:flex-row flex-col relative items-center justify-center w-full">
-        <Link href="/category/bollywood">
-          <div className="image-container mt-10">
-            <Image
-              width={1080}
-              height={1080}
-              layout="responsive" 
-              src={Bollywood}
-              className="grayscale cursor-pointer transition duration-300 ease-in-out transform hover:grayscale-0"
-              alt="Bollywood"
-            />{" "}
-            <p className="text-center mt-4 text-xl font-semibold">Bollywood</p>
-          </div>
-        </Link>
-
-        <Link href="/category/marvel">
-          <div className="image-container mt-10">
-            <Image
-              width={1080}
-              height={1080}
-              src={MarvelImg}
-              className="md:w-[350px] w-full grayscale cursor-pointer transition duration-300 ease-in-out transform hover:grayscale-0"
-              alt=""
-            />
-            <p className="text-center mt-4 text-xl font-semibold">Marvel</p>
-          </div>
-        </Link>
-
-        <Link href="/category/anime">
-          <div className="image-container mt-10">
-            <Image
-              width={1080}
-              height={1080}
-              src={Anime}
-              className="grayscale cursor-pointer transition duration-300 ease-in-out transform hover:grayscale-0"
-              alt=""
-            />
-            <p className="text-center mt-4 text-xl font-semibold">Anime</p>
-          </div>
-        </Link>
-
-        <Link href="/category/model">
-          <div className="image-container mt-10">
-            <Image
-              width={1080}
-              height={1080}
-              src={Models}
-              className="grayscale w-[350px] cursor-pointer transition duration-300 ease-in-out transform hover:grayscale-0"
-              alt=""
-            />{" "}
-            <p className="text-center mt-4 text-xl font-semibold">
-              Role Models
-            </p>
-          </div>
-        </Link>
-      </div>
+      <CardHoverEffectDemo />
     </div>
   );
 };
 
 export default CategoryPage;
+
+// <div className="flex gap-4 flex-wrap md:flex-row flex-col relative items-center justify-center w-full">
+// <Link href="/category/bollywood">
+//   <div className="image-container mt-10">
+//     <Image
+//       width={1080}
+//       height={1080}
+//       layout="responsive"
+//       src={Bollywood}
+//       className="grayscale cursor-pointer transition duration-300 ease-in-out transform hover:grayscale-0"
+//       alt="Bollywood"
+//     />{" "}
+//     <p className="text-center mt-4 text-xl font-semibold">Bollywood</p>
+//   </div>
+// </Link>
+
+// <Link href="/category/marvel">
+//   <div className="image-container mt-10">
+//     <Image
+//       width={1080}
+//       height={1080}
+//       src={MarvelImg}
+//       className="md:w-[350px] w-full grayscale cursor-pointer transition duration-300 ease-in-out transform hover:grayscale-0"
+//       alt=""
+//     />
+//     <p className="text-center mt-4 text-xl font-semibold">Marvel</p>
+//   </div>
+// </Link>
+
+// <Link href="/category/anime">
+//   <div className="image-container mt-10">
+//     <Image
+//       width={1080}
+//       height={1080}
+//       src={Anime}
+//       className="grayscale cursor-pointer transition duration-300 ease-in-out transform hover:grayscale-0"
+//       alt=""
+//     />
+//     <p className="text-center mt-4 text-xl font-semibold">Anime</p>
+//   </div>
+// </Link>
+
+// <Link href="/category/model">
+//   <div className="image-container mt-10">
+//     <Image
+//       width={1080}
+//       height={1080}
+//       src={Models}
+//       className="grayscale w-[350px] cursor-pointer transition duration-300 ease-in-out transform hover:grayscale-0"
+//       alt=""
+//     />{" "}
+//     <p className="text-center mt-4 text-xl font-semibold">
+//       Role Models
+//     </p>
+//   </div>
+// </Link>
+// </div>
