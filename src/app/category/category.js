140020f85916f80eba2b46b5041194149a04b561
Nilesh Pandey <99646982+NileshPandey05@users.noreply.github.com>
2024-04-08 07:24:38 +0000

    category

--- src/app/category/category.js
+++ src/app/category/category.js
@@ -1,19 +1,33 @@
-import { getAllFrames, getAllReport } from "@/firebase/functions";
-import Image from "next/image";
+import { getAllFrames ,} from "@/firebase/functions";
 import { useSearchParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
 const Cat = () => {
     const searchParams = useSearchParams();
     const category = searchParams.get("cat");
+    const [frames, setFrames] = useState([]);
 
     const renderFrames = async () => {
-        const frames = await getAllFrames()
-        console.log(frames.filter(item=> category === item.category))
+        try {
+            const framesData = await getAllFrames();
+            const filteredFrames = framesData.filter(item => category === item.category);
+            setFrames(filteredFrames);
+        } catch (error) {
+            console.error("Error fetching frames:", error);
+        }
     }
-    useEffect(() => { renderFrames() }, [])
+
+    useEffect(() => { renderFrames() }, [category]);
+
     return (
-        <div>{category}</div>
+        <div>
+            <h1>{category}</h1>
+            <div className="frame-container">
+                {frames.map((frame, index) => (
+                    <img key={index} src={frame.imageUrl} alt={`Frame ${index}`} />
+                ))}
+            </div>
+        </div>
     );
 };
 
