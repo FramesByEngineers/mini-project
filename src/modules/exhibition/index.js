a888db2b5e599d90df3d0443d6af6190756dd3b9
codietech <viveksahu2425@gmail.com>
2024-04-10 10:40:52 +0530

    exhibition functionality done

--- src/modules/exhibition/index.js
+++ src/modules/exhibition/index.js
@@ -7,9 +7,7 @@ const SingleExhibition = ({ exhibitionData }) => {
         <section className='max-w-5xl'>
 
             <div className='border border-gray-300 rounded-xl mt-12 mx-auto max-w-xl px-4 lg:w-80 lg:h-96 lg:ml-[200px] items-center'>
-
                 <img src={exhibitionData.imageURL} className='rounded-xl  lg:w-80 lg:h-[350px] object-contain' />
-
                 <p className='text-center text-lg font-normal font-serif lg:absolute lg:ml-[450px] lg:-mt-[350px]'>
                     {exhibitionData.title}
                 </p>
@@ -20,11 +18,9 @@ const SingleExhibition = ({ exhibitionData }) => {
                 <p className='font-light text-gray-700 lg:absolute lg:ml-[450px] lg:-mt-[250px] md:mr-[200px]'>
                     {exhibitionData.description}
                 </p>
-
                 <Button className='mx-auto md:-mt-12 lg:ml-[450px] lg:-mt-12 lg:w-96 text-white'>
                     Buy Ticket
                 </Button>
-
             </div>
         </section>
 
